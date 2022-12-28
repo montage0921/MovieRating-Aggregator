81d93254c452d2df687515510e123afba4e28dab
montage0921 <garyshi0921@gmail.com>
2022-12-28 14:38:36 -0500

    Finish JS Validation for Form

--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@ import "./style.css";
 
 const apiKey = `6d8db339`;
 
+const titleInput = document.querySelector(`#title`);
+const yearInput = document.querySelector(`#year`);
+const inputs = [titleInput, yearInput];
+const errorMsg = `this cannot be empty`;
+
 const searchBtn = document.querySelector(`#searchMovie`);
-console.log(searchBtn);
+
+searchBtn.addEventListener(`click`, function (e) {
+  e.preventDefault();
+
+  //Inputs must be filled
+  inputs.forEach((input) => {
+    if (input.validity.valueMissing)
+      input.nextElementSibling.textContent = errorMsg;
+    else input.nextElementSibling.textContent = ``;
+  });
+
+  let title = titleInput.value;
+  let year = yearInput.value;
+
+  if (!title || !year) return;
+});
