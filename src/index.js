297597a0af0e7ac66295e1a683ed2d771fe6ce6a
montage0921 <garyshi0921@gmail.com>
2022-12-28 16:26:26 -0500

    Finish Getting All Movie Info from APIs

--- src/index.js
+++ src/index.js
@@ -23,4 +23,51 @@ searchBtn.addEventListener(`click`, function (e) {
   let year = yearInput.value;
 
   if (!title || !year) return;
+
+  fetch(`https://api.wmdb.tv/api/v1/movie/search?q=${title}&year=${year}`)
+    .then((res) => res.json())
+    .then((res) => {
+      let doubanId = res[0].doubanId;
+      let imdbId = res[0].imdbId;
+
+      const doubanPromise = fetch(
+        `https://api.wmdb.tv/movie/api?id=${doubanId}`
+      );
+
+      const imdbPromise = fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&i=${imdbId}`,
+        {
+          mode: `cors`,
+        }
+      );
+
+      return Promise.all([doubanPromise, imdbPromise]);
+    })
+    .then((res) => Promise.all([res[0].json(), res[1].json()]))
+    .then((res) => {
+      const douban = res[0];
+      const imdb = res[1];
+
+      //Movie Information
+      const title = douban.data[0].name;
+      const director = douban.director[0].data[0].name;
+      const actors = douban.actor
+        .slice(0, 4)
+        .map((actor) => actor.data[0].name)
+        .join(`/`);
+      const genre = douban.data[0].genre;
+
+      //Movie Ratings
+      const doubanRating = douban.doubanRating;
+      const imdbRating = imdb.Ratings[0].Value;
+      const metaScore = imdb.Ratings[2].Value;
+      const rottenTomato = imdb.Ratings[1].Value;
+
+      //Movie poster
+      const poster = imdb.Poster;
+
+      //Movie ID
+      const doubanID = douban.doubanId;
+      const imdbID = imdb.imdbID;
+    });
 });
